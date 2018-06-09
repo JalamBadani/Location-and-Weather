b929b518af7a07283644663b40221c4aafaf4c35
Elahe Badani <badani@TFSDC.com>
2018-06-09 07:55:18 +0430

    first commit

---
+++ app/src/main/java/jam/badani/weather/MapsActivity.java
@@ -0,0 +1,119 @@
+package jam.badani.weather;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.gson.Gson;
+import com.loopj.android.http.AsyncHttpClient;
+import cz.msebera.android.httpclient.Header;
+import com.loopj.android.http.TextHttpResponseHandler;
+
+import java.util.List;
+
+import jam.badani.weather.Model.YahooWeather;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+
+    private GoogleMap mMap;
+    static int temperature;
+
+
+
+    SupportMapFragment mapFragment;
+    private GoogleMap.OnCameraIdleListener onCameraIdleListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setOnCameraIdleListener(onCameraIdleListener);
+
+
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                // Creating a marker
+                MarkerOptions markerOptions = new MarkerOptions();
+                // Setting the position for the marker
+                markerOptions.position(latLng);
+                // Setting the title for the marker.
+                // This will be displayed on taping the marker
+                markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+                // Clears the previously touched position
+                mMap.clear();
+                // Animating to the touched position
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                // Placing a marker on the touched position
+                mMap.addMarker(markerOptions);
+
+               // LatLng latLng = mMap.getCameraPosition().target;
+                Geocoder geocoder = new Geocoder(MapsActivity.this);
+
+                try {
+                    List<Address> addressList = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+                    if (addressList != null && addressList.size() > 0) {
+                        String locality = addressList.get(0).getAddressLine(0);
+                        //String country = addressList.get(0).getCountryName();
+                        if (!locality.isEmpty())
+                        Toast.makeText(MapsActivity.this,locality + "temperature: "+ getLocationTemp(locality) ,Toast.LENGTH_SHORT).show();
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+    }
+
+    private  int getLocationTemp(String city)
+    {
+        String url ="https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+city+"%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, new TextHttpResponseHandler() {
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String responseString) {
+
+                Gson gson = new Gson();
+                YahooWeather weather = gson.fromJson(responseString, YahooWeather.class);
+                String temp = weather.getQuery().getResults().getChannel().getItem().getCondition().getTemp();
+                MapsActivity.temperature=  Integer.parseInt(temp);
+
+            }
+        });
+
+        return  temperature;
+    }
+
+
+
+}
---
+++ app/src/main/java/jam/badani/weather/Model/Results.java
@@ -0,0 +1,21 @@
+
+package jam.badani.weather.Model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Results {
+
+    @SerializedName("channel")
+    @Expose
+    private Channel channel;
+
+    public Channel getChannel() {
+        return channel;
+    }
+
+    public void setChannel(Channel channel) {
+        this.channel = channel;
+    }
+
+}
